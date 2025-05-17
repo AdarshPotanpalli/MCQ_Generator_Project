f0cb00afbe585ed7d9e67eabf28ed366042c5db4
Adarsh Potanpalli <p.adarsh.24072001@gmail.com>
2025-05-17 21:44:39 +0200

    file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup # find package finds our created package
+
+# sets up the meta data for the package
+setup(
+    name="mcqgenerator",
+    version="0.0.1",
+    author="Adarsh Potanpalli",
+    author_email="p.adarsh.24072001@gmail.com",
+    install_requires=['openai','python-dotenv','streamlit','langchain[openai]','PyPDF2'],
+    packages=find_packages()
+)
